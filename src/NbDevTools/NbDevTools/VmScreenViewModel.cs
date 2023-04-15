71d5779cda307a3192089e9332e688526c154712
Nick <n.bilsby@hotmail.co.uk>
2023-04-15 20:31:07 +0100

    Got it working :)

--- src/NbDevTools/NbDevTools/VmScreenViewModel.cs
+++ src/NbDevTools/NbDevTools/VmScreenViewModel.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Management.Automation;
 
 namespace NbDevTools
 {
@@ -52,19 +53,100 @@ namespace NbDevTools
             }
         }
 
-        private void OnDrop(object e)
+        private void OnDrop(object sender)
         {
-            var args = (DragEventArgs)e;
+            var args = (DragEventArgs)sender;
+            //private void Drop(object sender, DragEventArgs e)
+            //{
             if (args.Data.GetDataPresent(DataFormats.FileDrop))
-            {
-                var files = (string[])args.Data.GetData(DataFormats.FileDrop);
-                if (files != null && files.Length > 0)
                 {
-                    var fileInfo = new FileInfo(files[0]);
-                    DroppedFileInfo = fileInfo;
-                    Debug.WriteLine(DroppedFileInfo.FullName);
+                    string[] files = (string[])args.Data.GetData(DataFormats.FileDrop);
+
+                    foreach (string file in files)
+                    {
+                        var fileInfo = new FileInfo(file);
+                        string command = $"Copy-VMFile 'Hadrian_Clean' -SourcePath '{fileInfo.FullName}' -DestinationPath 'C:\\Received\\{fileInfo.Name}' -CreateFullPath -FileSource Host";
+                        ExecuteCommandElevated(command);
+                    }
                 }
+            //}
+
+            //var args = (DragEventArgs)e;
+            //if (args.Data.GetDataPresent(DataFormats.FileDrop))
+            //{
+            //    var files = (string[])args.Data.GetData(DataFormats.FileDrop);
+            //    if (files != null && files.Length > 0)
+            //    {
+            //        var fileInfo = new FileInfo(files[0]);
+            //        DroppedFileInfo = fileInfo;
+            //        Debug.WriteLine(DroppedFileInfo.FullName);
+            //        SendFileToVM(DroppedFileInfo, "Hadrian_Clean");
+            //    }
+            //}
+        }
+
+        private void SendFileToVM(FileInfo file, string vmName)
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.AddCommand("ping");
+                ps.AddParameter("8.8.8.8");
+
+                ps.Invoke();
+
+                // Load the Hyper-V module
+                ps.AddCommand("Import-Module");
+                ps.AddParameter("Name", "Hyper-V");
+
+                ps.Invoke();
+
+
+                // Set the command to copy the file to the VM
+                ps.AddCommand("Copy-VMFile");
+                ps.AddParameter("VMName", vmName);
+                ps.AddParameter("FileSource", file.FullName);
+                ps.AddParameter("DestinationPath", "C:\\Received\\");
+                ps.AddParameter("CreateFullPath", true);
+
+                // Execute the command
+                ps.Invoke();
             }
         }
+
+        /// <summary>
+        /// We cant set <requestedExecutionLevel level="requireAdministrator" uiAccess="true" /> without changing security policy.
+        /// So better off launching a new process that will request elevation as necessary
+        /// </summary>
+        /// <param name="command"></param>
+        private void ExecuteCommandElevated(string command)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "powershell.exe";
+            startInfo.Arguments = $"-Command \"{command}\"";
+            startInfo.Verb = "runas"; // Run the process as administrator
+
+            startInfo.UseShellExecute = true;
+            startInfo.CreateNoWindow = false;
+            //startInfo.RedirectStandardOutput = true; // Redirect the output stream
+            //startInfo.RedirectStandardError = true; // Redirect the error stream
+            Process process = Process.Start(startInfo);
+
+            if (process == null) return;
+
+            // Create StreamReader objects to read the output and error streams
+            //StreamReader outputReader = process.StandardOutput;
+            //StreamReader errorReader = process.StandardError;
+
+            //// Read the output and error streams
+            //string output = outputReader.ReadToEnd();
+            //string error = errorReader.ReadToEnd();
+
+            //Debug.WriteLine($"Output: {output}");
+            //Debug.WriteLine($"Error: {error}");
+
+            //// Wait for the process to exit
+            //process.WaitForExit();
+        }
+
     }
 }
